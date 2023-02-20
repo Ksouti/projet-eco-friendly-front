9d5f60298be79518cab54051482db58cdef55632
AnoukFerrand <anoukfrd@gmail.com>
2023-02-20 19:04:30 +0100

    FIX: bug on images. Keep going on Hamburger composant

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,13 +1,14 @@
 import navBarLogo from './logo-copy.png';
 import navBarAvatar from './avatar-bear.png';
 import Hamburger from '../Hamburger';
+import "./styles.scss";
 
 function Navbar() {
   return (
     <div className="navbar">
       <Hamburger />
-      <img src={navBarLogo} alt="react logo" />
-      <img src={navBarAvatar} alt="react logo" />
+      <img src={navBarLogo} alt="logo" className="imgLogo" />
+      <img src={navBarAvatar} alt="avatar" className="imgAvatar" />
 
     </div>
   );
