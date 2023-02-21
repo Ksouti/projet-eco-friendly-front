c9a08ba71f2b4ba003ddebfca7345723fe921d7b
Kitsu <97291210+Ksouti@users.noreply.github.com>
2023-02-21 17:30:05 +0100

    FIX: Improved footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,22 +1,24 @@
-import footerLogoFb from './logo_fb.png';
-import footerLogoInsta from './logo_insta.png';
+import footerLogoFacebook from './logo_fb.png';
+import footerLogoInstagram from './logo_insta.png';
 import footerLogoTwitter from './logo_twitter.png';
 import './styles.scss';
 
 function Footer() {
   return (
     <footer className="footer">
-      <div className="footer-col">
+      <div className="footer-links">
         <ul>
-          <li><a href="#">© EcoFriendly</a></li>
-          <li><a href="#">Mentions légales</a></li>
           <li><a href="#">Contact</a></li>
+          <li><a href="#">Mentions légales</a></li>
         </ul>
-        <div className="social-links">
-          <a href="#"> <img src={footerLogoFb} alt="logo" /></a>
-          <a href="#"> <img src={footerLogoTwitter} alt="logo" /></a>
-          <a href="#"> <img src={footerLogoInsta} alt="logo" /></a>
-        </div>
+      </div>
+      <div className="footer-copyright">
+        <p>© EcoFriendly</p>
+      </div>
+      <div className="footer-logo">
+        <a href="#"> <img src={footerLogoTwitter} alt="logoTwitter" /></a>
+        <a href="#"> <img src={footerLogoInstagram} alt="logoInstagram" /></a>
+        <a href="#"> <img src={footerLogoFacebook} alt="logoFacebook" /></a>
       </div>
     </footer>
   );
