1edb82ee2372ce8101919610f1f331836fd6710d
AnoukFerrand <anoukfrd@gmail.com>
2023-02-21 15:43:48 +0100

    FEAT: Navbar Mobile done

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -10,6 +10,16 @@ function Navbar() {
       <img src={navBarLogo} alt="logo" className="imgLogo" />
       <img src={navBarAvatar} alt="avatar" className="imgAvatar" />
 
+      <nav>
+        <ul className="menuList">
+          <li className="menu homepageMenu"><a>Accueil</a></li>
+          <li className="menu mobilityMenu">Mobilité</li>
+          <li className="menu homeMenu">Maison</li>
+          <li className="menu healthMenu">Santé</li>
+          <li className="menu energyMenu">Energie</li>
+        </ul>
+      </nav>
+
     </div>
   );
 }
