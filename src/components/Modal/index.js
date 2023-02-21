ffc91321e1f761c304a0941f5edd89f94f1231a5
Alban Caré <alban.care@gmail.com>
2023-02-21 20:00:07 +0000

    FEAT: Added ability to close modal window
    - by clicking on the button
    - by clicking on the overlay

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,15 +1,25 @@
 import './styles.scss';
 
-export default function Modal({ children }) {
+export default function Modal({ onClose, open, children }) {
   return (
-    <div className="modal">
+    <div
+      className={open ? 'modal' : 'modal modal-hidden'}
+      onClick={() => onClose()}
+    >
       <div className="modal-content">
         <div className="modal-header">
-          <button type="button" className="btn-close" aria-label="Close">
+          <button
+            type="button"
+            className="btn-close"
+            aria-label="Close"
+            onClick={() => onClose()}
+          >
             <span aria-hidden="true">&times;</span>
           </button>
         </div>
-        <div className="modal-body">{children}</div>
+        <div className="modal-body" onClick={(e) => e.stopPropagation()}>
+          {children}
+        </div>
       </div>
     </div>
   );
