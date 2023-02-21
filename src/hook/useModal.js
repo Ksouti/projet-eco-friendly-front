c56bf200b5bde4034cec2f7193bee668804b0d98
Alban Caré <alban.care@gmail.com>
2023-02-21 20:28:51 +0000

    FEAT: custom Hook to manage the modal window

---
+++ src/hook/useModal.js
@@ -0,0 +1,14 @@
+import { useState } from 'react';
+
+export default function useModal() {
+  const [isShowing, setIsShowing] = useState(false);
+
+  const toggleModal = () => {
+    setIsShowing(!isShowing);
+  };
+
+  return {
+    isShowing,
+    toggleModal,
+  };
+}
