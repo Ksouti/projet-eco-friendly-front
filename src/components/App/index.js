f745ae9b5752d1f49eae620f18c3a9c22915bde2
AnoukFerrand <anoukfrd@gmail.com>
2023-02-22 13:58:51 +0100

    scss card component in progress

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Navbar from '../Navbar';
 import Footer from '../Footer';
 import Homepage from '../Homepage';
+import Card from '../Card';
 import NotFoundPage from '../NotFoundPage';
 
 import './styles.css';
@@ -13,6 +14,7 @@ function App() {
   return (
     <div className="app">
       <Navbar />
+      <Card />
       <Routes>
         <Route path="/" element={<Homepage />} />
         {/* <Route path="/categories/:slug" element={<CategoryPage />} />
