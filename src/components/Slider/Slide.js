eeb1a2d7003de3e60578c38707cf0dead217a8bb
Alban Caré <alban.care@gmail.com>
2023-02-22 23:09:51 +0000

    FEAT: Component representing the content of the slides

---
+++ src/components/Slider/Slide.js
@@ -0,0 +1,31 @@
+import PropTypes from 'prop-types';
+
+import { Link } from 'react-router-dom';
+
+export default function Slide({ title, content, slug, picture, tag }) {
+  return (
+    <div className="slide">
+      <div className="slide-inner">
+        <div className="slide-image">
+          <img src={picture} alt={`Image de l'article ${{ title }}`} />
+        </div>
+        <div className="slide-body">
+          <h5 className="slide-title">{title}</h5>
+          <span className="slide-tag">{tag}</span>
+          <p className="slide-text">{content}</p>
+          <Link to={`/${slug}`} className="slide-link">
+            Consulter l'article
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+Slide.propTypes = {
+  title: PropTypes.string.isRequired,
+  content: PropTypes.string.isRequired,
+  slug: PropTypes.string.isRequired,
+  picture: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+};
