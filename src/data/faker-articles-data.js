4d93939b4385a82ad93fd449a4043af18af00184
Alban Caré <alban.care@gmail.com>
2023-02-22 23:07:20 +0000

    FEAT: Adding a false data set

---
+++ src/data/faker-articles-data.js
@@ -0,0 +1,38 @@
+export default [
+  {
+    id: 1,
+    title: 'Lorem, ipsum dolor sit amet consectetur',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. In odio voluptatum cumque quod obcaecati architecto molestias, aliquam, iusto recusandae aliquid modi, hic quam magni. Obcaecati pariatur expedita, ipsam illum vitae iusto quaerat iure hic officiis? Hic eligendi nulla, eum ut optio quasi ipsum quibusdam voluptate iusto corrupti dolor omnis et necessitatibus illo! Amet itaque aspernatur nemo ipsam fugiat similique nisi corrupti quas rerum vitae quos incidunt saepe recusandae modi, tempora sed et magni laboriosam, inventore qui facere alias culpa nihil. Expedita debitis quisquam dolor error?',
+    slug: 'lorem-ipsum-dolor-sit-amet-consectetur',
+    picture: 'https://picsum.photos/id/425/640/800',
+    category: 'consectetur',
+  },
+  {
+    id: 2,
+    title: 'Delectus quia itaque necessitatibus',
+    content:
+      'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod perferendis veniam totam, fugit ullam, dolor illo, dolorum incidunt laborum officia adipisci corporis officiis repellat pariatur! Consequuntur, aut aperiam tempore corrupti modi ipsum perspiciatis explicabo nulla saepe porro odit maxime hic, reiciendis amet facere, voluptatum voluptate quidem veritatis quae? Cumque quidem explicabo, possimus natus veniam et illo earum nemo quod? Architecto, possimus inventore. Asperiores repellendus ea corporis cupiditate dolores accusamus molestiae corrupti consequuntur adipisci! Ipsum dolorem, nulla voluptatibus corporis fugit voluptate debitis non iure tempora. Deserunt ut ipsam, at cum quam magni est doloribus placeat, necessitatibus reiciendis alias porro blanditiis in, vitae eos mollitia voluptate eum et! Nulla at harum quisquam inventore quidem.',
+    slug: 'delectus-quia-itaque-necessitatibus',
+    picture: 'https://picsum.photos/id/612/640/800',
+    category: 'elit',
+  },
+  {
+    id: 3,
+    title: 'Commodi, culpa fugit,',
+    content:
+      'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit ipsum perspiciatis nobis hic quasi aliquam veritatis quaerat eaque rerum, labore reprehenderit quas ad doloremque, architecto asperiores molestias commodi! Nisi harum expedita vero eos nam aspernatur nihil tempore natus quaerat dolorum cupiditate enim eaque dolore culpa illo et, eius doloremque repudiandae delectus minus quisquam dicta! Maiores sed accusamus quos labore aliquid, eaque eveniet corrupti molestiae consequatur? Ducimus perspiciatis aliquid officia harum.',
+    slug: 'Commodi-culpa-fugit,',
+    picture: 'https://picsum.photos/id/444/640/800',
+    category: 'necessitatibus',
+  },
+  {
+    id: 4,
+    title: 'Consectetur adipisicing ',
+    content:
+      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil exercitationem quia nobis fugiat ipsam ratione enim atque rerum. Eveniet, earum architecto doloribus nihil libero dolor at iste. Aspernatur, neque dolor obcaecati ut repudiandae eos placeat dolorum eius vero tenetur non reiciendis quam alias hic veniam molestias doloremque sunt impedit quo. Nihil aperiam nulla, assumenda doloribus repudiandae harum, cum quod animi non aliquam repellendus et optio ipsum hic natus illo in architecto amet at! Adipisci, fuga eius error quisquam ab nam magni, voluptate aliquam necessitatibus ad dicta quasi voluptatibus suscipit vitae nostrum id nulla similique perspiciatis totam! Obcaecati temporibus est magni incidunt neque animi dolorem consequatur delectus facilis doloribus, fugit autem asperiores nulla vero, assumenda doloremque tempora? Ipsa numquam nihil praesentium rem magnam optio consequuntur vel, aliquam nemo impedit eveniet, maiores reiciendis eligendi fuga eos soluta ex sunt aut beatae perspiciatis nobis incidunt. Ipsam molestias repudiandae error similique eum necessitatibus fugiat beatae qui nemo inventore dolore, fugit sequi, voluptatum, provident possimus quaerat non blanditiis quo corrupti accusantium earum mollitia sed laborum! Nesciunt, esse quae vero nisi doloremque reprehenderit. Temporibus adipisci libero veniam totam! Voluptas nesciunt cupiditate nulla ratione nihil eius incidunt doloribus enim praesentium ipsa error soluta iure qui repellat impedit quae voluptate, possimus dicta commodi quam ab accusamus explicabo temporibus vero?',
+    slug: 'consectetur-adipisicing ',
+    picture: 'https://picsum.photos/id/128/640/800',
+    category: 'itaque',
+  },
+];
