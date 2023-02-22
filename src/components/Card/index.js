46978813dbe798d832da26f88f753b31de679c22
AnoukFerrand <anoukfrd@gmail.com>
2023-02-22 19:33:49 +0100

    FEAT: Card composant horizontal version done

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,24 +1,39 @@
+import PropTypes from 'prop-types';
 import Page from '../Page';
 
+
 import './styles.scss';
-import imgArticle1 from '../../assets/imgEco.jpg';
+// import imgArticle from '../../assets/imgEco.jpg';
 
 
-function Card() {
+function Card({ format, image }) {
   return (
     <Page>
-      <div className="card">
-        <div className="image">
-          <img src={imgArticle1} alt="imageArticle1" className="imageArticle1" />
-        </div>
+      <div className="card" data-format={format}>
+      {/* condition si il y a une image ou pas */}
+        { image &&
+          (
+          <div className="image">
+            <img src="https://picsum.photos/id/425/640/800" alt="image de l'article - titreArticle" className="image-article" />
+          </div>
+          )}
         <div className="informations">
-          <h3 className="titleArticle">Le titre de l'article</h3>
-          <p className="summaryArticle">Cheese on toast airedale the big cheese. Danish fontina cheesy grin airedale danish fontina taleggio the big cheese macaroni cheese port-salut. Danish fontina cheesy grin airedale...</p>
+          <h3 className="title-article">Le titre de l'article</h3>
+          <p className="summary-article">Cheese on toast airedale the big cheese. Danish fontina cheesy grin airedale danish fontina taleggio the big cheese macaroni cheese port-salut. Danish fontina cheesy grin airedale...</p>
         </div>
       </div>
     </Page>
 
   );
 }
+
+Card.propTypes = {
+  format: PropTypes.string,
+  image: PropTypes.bool,
+};
+Card.defaultProps = {
+  format: '',
+  image: true,
+};
 
 export default Card;
