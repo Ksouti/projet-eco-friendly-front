f8abb48af24234bf6961c02c19c20bcb2884d0c2
AnoukFerrand <anoukfrd@gmail.com>
2023-02-22 15:06:18 +0100

    Card composant done on vertical position

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,16 +1,19 @@
 import Page from '../Page';
+
 import './styles.scss';
+import imgArticle1 from '../../assets/imgEco.jpg';
+
 
 function Card() {
   return (
     <Page>
       <div className="card">
         <div className="image">
-          <img src="" alt="imageArticle" className="imageArticle" />
+          <img src={imgArticle1} alt="imageArticle1" className="imageArticle1" />
         </div>
         <div className="informations">
-          <title>Le titre de l'article</title>
-          <p>Le résumé de l'article</p>
+          <h3 className="titleArticle">Le titre de l'article</h3>
+          <p className="summaryArticle">Cheese on toast airedale the big cheese. Danish fontina cheesy grin airedale danish fontina taleggio the big cheese macaroni cheese port-salut. Danish fontina cheesy grin airedale...</p>
         </div>
       </div>
     </Page>
