9f9f7705dbce03af39b113af5ab375acf6fc974f
Alban Caré <alban.care@gmail.com>
2023-02-23 18:46:15 +0000

    FEAT: Using the Settings and Table components in the UserSettingsPage

--- src/components/UserSettingsPage/index.js
+++ src/components/UserSettingsPage/index.js
@@ -1,7 +1,20 @@
 import Page from '../Page';
+import Table from './Table';
+import Settings from './Settings';
 
 import './styles.scss';
 
+import user from '../../data/single-user';
+
 export default function UserSettingsPage() {
-  return <Page>{/* TODO */}</Page>;
+  return (
+    <Page>
+      <section className="section">
+        <Settings user={user} />
+      </section>
+      <section className="section">
+        <Table items={user.advices} />
+      </section>
+    </Page>
+  );
 }
