3001161b4e439990a3b0a4be3d572951a1021462
Alban Caré <alban.care@gmail.com>
2023-02-24 22:48:21 +0100

    FEAT: Preparing the ArticlePage component

---
+++ src/components/ArticlePage/index.js
@@ -0,0 +1,15 @@
+import Page from '../Page';
+
+import './styles.scss';
+
+function ArticlePage() {
+  return (
+    <Page>
+      <div className="article">
+        <p>Page d'un article</p>
+      </div>
+    </Page>
+  );
+}
+
+export default ArticlePage;
