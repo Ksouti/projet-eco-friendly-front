2344b7817d1dbcf7f1b6c178a28ac54313b77a32
Alban Caré <alban.care@gmail.com>
2023-02-24 23:33:49 +0100

    FEAT: App component update
    - preparation for the support of a Loader component
    - end of routing configuration

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -7,28 +7,39 @@ import UserSettingsPage from '../UserSettingsPage/index';
 import NotFoundPage from '../NotFoundPage';
 import AddAdvicePage from '../AddAdvicePage';
 import CategoryPage from '../CategoryPage';
+import ArticlePage from '../ArticlePage';
+import AdvicePage from '../AdvicePage';
+import Loader from '../Loader';
 
 import './styles.css';
 
 function App() {
-  // loader
+  const isLoaded = true;
+
+  if (!isLoaded) {
+    return <Loader />;
+  }
 
   return (
     <div className="app">
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/categories" element={<CategoryPage />} />
-        <Route path="/categories" element={<CategoryPage />} />
-        {/* <Route path="/categories/:slug" element={<CategoryPage />} /> */}
-        {/* <Route path="/articles/:slug" element={<ArticlePage />} /> */}
-        {/* <Route path="/conseils/:slug" element={<AdvicePage />} /> */}
-        <Route path="/utilisateurs" element={<UserSettingsPage />} />
-        {/* Provisional choice to be reviewed as a team */}
-        <Route path="/conseils/ajouter" element={<AddAdvicePage />} />
-        {/* <Route path="/conseils/:slug/editer" element={<AddAdvicePage />} /> */}
-        <Route path="*" element={<NotFoundPage />} />
-      </Routes>
+      <header>
+        <Navbar />
+      </header>
+      {isLoaded && (
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/categories/:name" element={<CategoryPage />} />
+          <Route path="/articles/:slug" element={<ArticlePage />} />
+          <Route path="/conseils/:slug" element={<AdvicePage />} />
+          <Route
+            path="/utilisateurs/:nickname"
+            element={<UserSettingsPage />}
+          />
+          <Route path="/conseils/ajouter" element={<AddAdvicePage />} />
+          <Route path="/conseils/:slug/editer" element={<AddAdvicePage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      )}
       <Footer />
     </div>
   );
