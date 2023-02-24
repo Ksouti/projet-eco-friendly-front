e5420b76d1e3e3b2d32ed75a3a6ea26cbf7188c9
Alban Caré <alban.care@gmail.com>
2023-02-24 23:24:04 +0100

    FEAT: Preparing the Loader component

---
+++ src/components/Loader/index.js
@@ -0,0 +1,11 @@
+import './styles.scss';
+
+function Loader() {
+  return (
+    <div className="loader">
+      <div className="spinner">{/* todo */}</div>
+    </div>
+  );
+}
+
+export default Loader;
