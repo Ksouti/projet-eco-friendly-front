c48cc6adecea5a7e14e8a153d13c22f3597353ed
Alban Caré <alban.care@gmail.com>
2023-02-25 00:19:52 +0100

    FEAT: Preparing the LegalNotice component

---
+++ src/components/ LegalNoticePage/index.js
@@ -0,0 +1,15 @@
+import Page from '../Page';
+
+import './styles.scss';
+
+function LegalNoticePage() {
+  return (
+    <Page>
+      <div className="legal-notice">
+        <p>Page des mentions légales</p>
+      </div>
+    </Page>
+  );
+}
+
+export default LegalNoticePage;
