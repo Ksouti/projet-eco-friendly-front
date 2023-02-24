19d260f72c20ba0ac9588161dea7201584ac3b40
Alban Caré <alban.care@gmail.com>
2023-02-24 22:51:33 +0100

    FEAT: Preparing the AdvicePage component

---
+++ src/components/AdvicePage/index.js
@@ -0,0 +1,15 @@
+import Page from '../Page';
+
+import './styles.scss';
+
+function AdvicePage() {
+  return (
+    <Page>
+      <div className="advice">
+        <p>Page d'un conseil</p>
+      </div>
+    </Page>
+  );
+}
+
+export default AdvicePage;
