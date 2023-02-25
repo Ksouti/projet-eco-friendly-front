06e67d508e5b9f220b4ee531095b0951f9fd9837
Alban Caré <alban.care@gmail.com>
2023-02-25 04:28:28 +0100

    FEAT: Installation of slider and card components

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -1,11 +1,34 @@
 import Page from '../Page';
+import Slider from '../Slider';
+import Card from '../Card';
 
 import './styles.scss';
+
+import articles from '../../data/lastFourArticles';
+import advices from '../../data/lastFourAdvices';
 
 function HomePage() {
   return (
     <Page>
-      <div className="homepage">{/* todo */}</div>
+      <div className="homepage">
+        <section className="advices">
+          <h2 className="advices-title">Suivez vos conseils</h2>
+          <div className="card-wrapper">
+            {advices.map((advice) => (
+              <Card
+                key={advice.id}
+                title={advice.title}
+                content={advice.content}
+                picture={advice.picture}
+                category={advice.category}
+              />
+            ))}
+          </div>
+        </section>
+        <section className="articles">
+          <Slider slides={articles} />
+        </section>
+      </div>
     </Page>
   );
 }
