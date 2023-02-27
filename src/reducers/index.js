5674dda45d4d7dacc8c5a73d8c56577213de0e35
Alban Caré <alban.care@gmail.com>
2023-02-27 18:59:14 +0100

    FEAT: Updated App and HomePage components to use state

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 
 import userReducer from './user';
 import advicesReducer from './advices';
-import articlesReducer from './advices';
+import articlesReducer from './articles';
 
 const rootReducer = combineReducers({
   user: userReducer,
