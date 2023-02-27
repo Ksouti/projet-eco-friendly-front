7a7c9a56ae2997a5af80482f11e58ca8b71449a0
Alban Caré <alban.care@gmail.com>
2023-02-27 21:08:10 +0100

    FEAT: Fetching user data  with Redux
    - Added actions: login, logout, authentication succes & authentication failed
    - Added user login action in the Middleware (fake data)
    - Initialized data in the state and added authentication & logout actions

--- src/middlewares/userMiddleware.js
+++ src/middlewares/userMiddleware.js
@@ -1,5 +1,13 @@
+import { USER_LOGIN, userAuthenticationSuccess } from '../actions/user';
+
+import user from '../data/single-user'; // dev only
+
 const userMiddleware = (store) => (next) => (action) => {
   switch (action.type) {
+    case USER_LOGIN:
+      console.log('Middleware: user authentication'); // dev only
+      store.dispatch(userAuthenticationSuccess(user)); // dev only
+      break;
     default:
   }
   next(action);
