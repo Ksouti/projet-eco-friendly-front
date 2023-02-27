00ac7cb83339480bc53ce1c75d140e5efdd9f11f
Alban Caré <alban.care@gmail.com>
2023-02-27 18:51:30 +0100

    FEAT: Fetching advices data with Redux
    - Added actions: loading and fetch
    - Implementation of the loading action in the Middleware
    - Initialization of the state and addition of the fetch action

--- src/middlewares/advicesMiddleware.js
+++ src/middlewares/advicesMiddleware.js
@@ -1,5 +1,13 @@
+import { LOADING_ADVICES_DATA, fetchAdvicesFromApi } from '../actions/advices';
+
+import advices from '../data/advices'; // dev only
+
 const advicesMiddleware = (store) => (next) => (action) => {
   switch (action.type) {
+    case LOADING_ADVICES_DATA:
+      console.log('Middleware: loading advices data'); // dev only
+      store.dispatch(fetchAdvicesFromApi(advices)); // dev only
+      break;
     default:
   }
   next(action);
--- src/reducers/advices.js
+++ src/reducers/advices.js
@@ -1,8 +1,18 @@
-export const initialState = {};
+import { FETCH_ADVICES_FROM_API } from '../actions/advices';
+
+export const initialState = {
+  data: [],
+  isLoaded: false,
+};
 
 const reducer = (state = initialState, action = {}) => {
   switch (action.type) {
-    // TODO
+    case FETCH_ADVICES_FROM_API:
+      return {
+        ...state,
+        data: action.data,
+        isLoaded: true,
+      };
     default:
       return state;
   }
