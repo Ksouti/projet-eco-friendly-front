70870dffbc9cdcff87570edb018e9eded7909e4f
Alban Caré <alban.care@gmail.com>
2023-02-28 00:08:20 +0100

    FEAT: Using Modal in component user Settings Page

--- src/components/UserSettingsPage/Account/index.js
+++ src/components/UserSettingsPage/Account/index.js
@@ -1,38 +1,60 @@
-import { useContext } from 'react';
+import { useDispatch } from 'react-redux';
 import PropTypes from 'prop-types';
-import { ModalContext } from '../../../context/ModalContext';
+import { openModal } from '../../../actions/common';
 
-import Modal from '../../Modal';
 import Button from '../../Button';
-// import FormNickname from '../../Form/UserSettingsForm/FormNickname';
-// import FormEmail from '../../Form/UserSettingsForm/FormEmail';
-// import FormFullname from '../../Form/UserSettingsForm/FormFullname';
-// import FormPassword from '../../Form/UserSettingsForm/FormPassword';
-// import FormDeleteAccount from '../../Form/UserSettingsForm/FormDeleteAccount';
-// import FormDeleteAdvice from '../../Form/UserSettingsForm/FormDeleteAdvice';
+import FormNickname from '../../Form/UserSettingsForm/FormNickname';
+import FormEmail from '../../Form/UserSettingsForm/FormEmail';
+import FormFullname from '../../Form/UserSettingsForm/FormFullname';
+import FormPassword from '../../Form/UserSettingsForm/FormPassword';
+import FormDeleteAccount from '../../Form/UserSettingsForm/FormDeleteAccount';
+import FormDeleteAdvice from '../../Form/UserSettingsForm/FormDeleteAdvice';
 import FormAvatar from '../../Form/UserSettingsForm/FormAvatar';
 
 import './styles.scss';
 
 export default function Account({ user }) {
-  const { toggleModal } = useContext(ModalContext);
+  const dispatch = useDispatch();
+
+  /**
+   * Retunrs the content of the modal window
+   *
+   * @param {String} contentName
+   * @returns
+   */
+  const modalContent = (contentName) => {
+    switch (contentName) {
+      case 'nickname':
+        return <FormNickname nickname={user.nickname} />;
+      case 'email':
+        return <FormEmail email={user.email} />;
+      case 'fullname':
+        return (
+          <FormFullname lastname={user.lastname} firstname={user.firstname} />
+        );
+      case 'password':
+        return <FormPassword />;
+      case 'delete-account':
+        return <FormDeleteAccount />;
+      case 'delete-advice':
+        return <FormDeleteAdvice />;
+      case 'avatar':
+        return <FormAvatar />;
+      default:
+        return null;
+    }
+  };
+
+  const toggleModal = (e) => {
+    const { name } = e.target;
+    dispatch(openModal(modalContent(name)));
+  };
 
   return (
     <section className="account">
-      <Modal>
-        {/* <FormNickname nickname={user.nickname} /> */}
-        {/* <FormEmail email={user.email} /> */}
-        {/* <FormFullname lastname={user.lastname} firstname={user.firstname} /> */}
-        {/* <FormPassword /> */}
-        {/* <FormDeleteAccount /> */}
-        {/* <FormDeleteAdvice /> */}
-        <FormAvatar />
-      </Modal>
-
       <div className="account-inner">
         <div className="account-avatar">
           <img src={user.avatar} alt="Avatar par defaut" className="avatar" />
-          {/* Opens a modal window for modifying avatar with 5 default avatars */}
           <Button
             type="button"
             color="link-primary"
@@ -48,7 +70,6 @@ export default function Account({ user }) {
               <h5 className="account-title">Pseudo</h5>
               <p className="account-text nickname">{user.nickname}</p>
             </div>
-            {/* Opens a modal window for modifying the nickname edit modal */}
             <Button
               type="button"
               color="primary"
@@ -63,7 +84,6 @@ export default function Account({ user }) {
               <h5 className="account-title">Email</h5>
               <p className="account-text email">{user.email}</p>
             </div>
-            {/* Opens a modal window for modifying an email edit modal */}
             <Button
               type="button"
               color="primary"
@@ -78,7 +98,6 @@ export default function Account({ user }) {
               <h5 className="account-title">Nom et Prénom</h5>
               <p className="account-text fullname">{`${user.lastname} ${user.firstname}`}</p>
             </div>
-            {/* Opens a modal window for modifying first and last name */}
             <Button
               type="button"
               color="primary"
@@ -90,7 +109,6 @@ export default function Account({ user }) {
           </div>
         </div>
         <div className="account-controllers">
-          {/* Opens a modal window for modifying password */}
           <Button
             type="button"
             color="primary"
@@ -100,7 +118,6 @@ export default function Account({ user }) {
             Changer de mot de passe
           </Button>
           <div className="account-warning-zone">
-            {/* Opens a modal window for delete user account */}
             <Button
               type="button"
               color="outline-secondary"
