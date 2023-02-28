782915a793e64f58c84973b7f1aba77f61c3841c
Alban Caré <alban.care@gmail.com>
2023-02-28 22:26:59 +0100

    FEAT: Added an action to listen to changes in input type fields

--- src/actions/common.js
+++ src/actions/common.js
@@ -1,9 +1,18 @@
+/**
+ * Loading categories data action
+ * @returns {object} : the action
+ */
 export const LOADING_CATEGORIES_DATA = 'LOADING_CATEGORIES_DATA';
 
 export const loadingCategoriesData = () => ({
   type: LOADING_CATEGORIES_DATA,
 });
 
+/**
+ * Fetch categories data action
+ * @param {object} data : the data to fetch
+ * @returns {object} : the action
+ */
 export const FETCH_CATEGORIES_FROM_API = 'FETCH_CATEGORIES_FROM_API';
 
 export const fetchCategoriesFromApi = (data) => ({
@@ -11,6 +20,12 @@ export const fetchCategoriesFromApi = (data) => ({
   data,
 });
 
+/**
+ * Modal opening  action
+ *
+ * @param {ReactNodeLike} modalContent
+ * @returns {object} : the action
+ */
 export const OPEN_MODAL = 'OPEN_MODAL';
 
 export const openModal = (modalContent) => ({
@@ -18,8 +33,27 @@ export const openModal = (modalContent) => ({
   modalContent,
 });
 
+/**
+ * Modal closing action
+ * @returns {object} : the action
+ */
 export const CLOSE_MODAL = 'CLOSE_MODAL';
 
 export const closeModal = () => ({
   type: CLOSE_MODAL,
+});
+
+/**
+ * Listen to the change in input type fields
+ *
+ * @param {string} value : the value of the input
+ * @param {string} identifier : the name of the input
+ * @returns {object} : the action
+ */
+export const ON_INPUT_CHANGE = 'ON_INPUT_CHANGE';
+
+export const OnInputChange = (value, identifier) => ({
+  type: ON_INPUT_CHANGE,
+  value,
+  identifier,
 });
