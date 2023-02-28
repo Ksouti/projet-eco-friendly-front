987c0465fe45365e1fe3beb05ca1d6d616ce6c20
Alban Caré <alban.care@gmail.com>
2023-03-01 00:07:29 +0100

    FEAT: Added user registration middleware

--- src/middlewares/userMiddleware.js
+++ src/middlewares/userMiddleware.js
@@ -1,4 +1,11 @@
-import { USER_LOGIN, userAuthenticationSuccess } from '../actions/user';
+/* eslint-disable no-case-declarations */
+import {
+  USER_LOGIN,
+  userAuthenticationSuccess,
+  USER_REGISTER,
+  userRegisterSuccess,
+  // userAuthenticationError,
+} from '../actions/user';
 
 import user from '../data/single-user'; // dev only
 
@@ -7,6 +14,34 @@ const userMiddleware = (store) => (next) => (action) => {
     case USER_LOGIN:
       console.log('Middleware: user authentication'); // dev only
       store.dispatch(userAuthenticationSuccess(user)); // dev only
+      break;
+    case USER_REGISTER:
+      console.log('Middleware: user registration'); // dev only
+      const data = {
+        email: store.getState().user.email,
+        password: store.getState().user.password,
+        nickname: store.getState().user.nickname,
+        firstname: store.getState().user.firstname,
+        lastname: store.getState().user.lastname,
+      };
+      store.dispatch(userRegisterSuccess(data)); // dev only
+
+      // axios
+      //   .post('http://localhost:3000/api/users/register', {
+      //     email: store.getState().user.email,
+      //     password: store.getState().user.password,
+      //     nickname: store.getState().user.nickname,
+      //     firstname: store.getState().user.firstname,
+      //     lastname: store.getState().user.lastname,
+      //   })
+      //   .then((response) => {
+      //     console.log(response); // dev only
+      //     store.dispatch(userRegisterSuccess(response.data));
+      //   })
+      //   .catch((error) => {
+      //     console.log(error); // dev only
+      //     store.dispacth(userAuthenticationError(error));
+      //   });
       break;
     default:
   }
