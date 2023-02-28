4ef7065d44e7b06922c4e8c77cc4d3314512f2ba
Kitsu <97291210+Ksouti@users.noreply.github.com>
2023-02-28 02:59:31 +0100

    FEAT: Modernization of the 404 with the latest components

--- src/components/NotFoundPage/index.js
+++ src/components/NotFoundPage/index.js
@@ -1,11 +1,19 @@
 import './styles.scss';
+import RichTextEditor from '../RichTextEditor';
+import Page from '../Page';
+import Input from '../Field/Input';
+import Button from '../Button';
 
 const NotFoundPage = () => (
-  <div className="not-found-container">
-    <h1>404</h1>
-    <p>Page non trouvée</p>
-    <a href="/">Retour à l'acceuil</a>
-  </div>
+  <Page>
+    <div className="not-found-container">
+      <h1>404</h1>
+      <p>Page non trouvée</p>
+      <Button color="primary">
+        Retourner à l'accueil
+      </Button>
+    </div>
+  </Page>
 );
 
 export default NotFoundPage;
