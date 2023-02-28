7f1bb573d50a186695c1a0c6495649aceec9a398
Alban Caré <alban.care@gmail.com>
2023-02-28 21:42:00 +0100

    FEAT: Fetching last four advices data with Redux
    - Added actions: loading and fetch
    - Implementation of the loading action in the Middleware
    - Initialization of the state and addition of the fetch action

--- src/reducers/advices.js
+++ src/reducers/advices.js
@@ -1,8 +1,12 @@
-import { FETCH_ADVICES_FROM_API } from '../actions/advices';
+import {
+  FETCH_ADVICES_FROM_API,
+  FETCH_LAST_FOUR_ADVICES_FROM_API,
+} from '../actions/advices';
 
 export const initialState = {
   data: [],
   isLoaded: false,
+  lastFourAdvices: [],
 };
 
 const reducer = (state = initialState, action = {}) => {
@@ -11,6 +15,12 @@ const reducer = (state = initialState, action = {}) => {
       return {
         ...state,
         data: action.data,
+        isLoaded: true,
+      };
+    case FETCH_LAST_FOUR_ADVICES_FROM_API:
+      return {
+        ...state,
+        lastFourAdvices: action.data,
         isLoaded: true,
       };
     default:
