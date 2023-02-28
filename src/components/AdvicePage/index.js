79e52b553e37840a0357964c41eb8b4b445ccb9a
AnoukFerrand <anoukfrd@gmail.com>
2023-02-28 17:39:28 +0100

    FEAT: add slug

--- src/components/AdvicePage/index.js
+++ src/components/AdvicePage/index.js
@@ -1,8 +1,18 @@
+import { Navigate, useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+import { findItem } from '../../utils';
+
 import Page from '../Page';
 
 import './styles.scss';
 
 function AdvicePage() {
+  const { slug } = useParams();
+
+  const advice = useSelector((state) => findItem(state.advices.data, slug));
+  console.log(advice);
+
   return (
     <Page>
       <div className="advice">
