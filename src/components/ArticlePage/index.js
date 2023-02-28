c42266293f122db2e48bd905920cede7c03f3d61
AnoukFerrand <anoukfrd@gmail.com>
2023-02-28 16:31:28 +0100

    FEAT: add slug

--- src/components/ArticlePage/index.js
+++ src/components/ArticlePage/index.js
@@ -1,8 +1,18 @@
+import { Navigate, useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+import { findItem } from '../../utils';
+
 import Page from '../Page';
 
 import './styles.scss';
 
 function ArticlePage() {
+  const { slug } = useParams();
+
+  const article = useSelector((state) => findItem(state.articles.data, slug));
+  console.log(article);
+
   return (
     <Page>
       <div className="article">
