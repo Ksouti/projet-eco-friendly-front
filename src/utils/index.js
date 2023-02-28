56f4ce1050e7df2f659d2bfb5ca26111917f4ba4
AnoukFerrand <anoukfrd@gmail.com>
2023-02-28 15:08:04 +0100

    FIX: fix function to find an item by category

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,15 +1,7 @@
 /* eslint-disable import/prefer-default-export */
 
-/**
- * Find element in array
- *
- * @param {Array} items - Array of data
- * @param {string} Element - Element to find
- * @param {string} param - Parameter to find (like 'id', 'name', 'slug')
- * @returns {Object}
- */
-function findElementInArray(items, element, param) {
-  return items.find((item) => item[param] === element);
+function findItemsByCategory(items, searchedSlug) {
+  return items.filter((item) => item.category.slug === searchedSlug);
 }
 
-export { findElementInArray };
+export { findItemsByCategory };
