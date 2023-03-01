35a483913a0355d1df7627a6e0cececcb47f8400
Alban Caré <alban.care@gmail.com>
2023-03-01 15:38:56 +0100

    FEAT: Added global config file - baseUrl: url of the front site
    - apiBaseUrl: url to the api
    
    - socialMedia: links and url of the logo to the assets of the server
    
    - defaultNavLink:
    internal link by default in case of failure to load in the api

---
+++ src/config.js
@@ -0,0 +1,44 @@
+/* eslint-disable import/prefer-default-export */
+export default {
+  baseUrl: 'http://vps-79770841.vps.ovh.net',
+  apiBaseUrl: 'http://vps-79770841.vps.ovh.net/api',
+  socialMedia: [
+    {
+      name: 'Instagram',
+      icon: 'http://vps-79770841.vps.ovh.net/assets/logos/instagram.png',
+      url: '',
+    },
+    {
+      name: 'Twitter',
+      icon: 'http://vps-79770841.vps.ovh.net/assets/logos/twitter.png',
+      url: '',
+    },
+    {
+      name: 'Facebook',
+      icon: 'http://vps-79770841.vps.ovh.net/assets/logos/facebook.png',
+      url: '',
+    },
+  ],
+  defaultNavLinks: [
+    {
+      name: 'Accueil',
+      url: '/',
+    },
+    {
+      name: 'Mobilité',
+      url: 'categories/mobilite',
+    },
+    {
+      name: 'Maison',
+      url: 'categories/maison',
+    },
+    {
+      name: 'Santé',
+      url: 'categories/sante',
+    },
+    {
+      name: 'Energie',
+      url: 'categories/energie',
+    },
+  ],
+};
