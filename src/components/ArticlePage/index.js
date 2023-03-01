b2384c22a573d81467d38f050dfb5c270c773b22
AnoukFerrand <anoukfrd@gmail.com>
2023-03-01 18:22:40 +0100

    FEAT: ArticlePage JSX done

--- src/components/ArticlePage/index.js
+++ src/components/ArticlePage/index.js
@@ -1,9 +1,9 @@
 /* eslint-disable function-paren-newline */
 /* eslint-disable implicit-arrow-linebreak */
-import { useParams } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+// import { useParams } from 'react-router-dom';
+// import { useSelector } from 'react-redux';
 
-import { findItem, findItemsByCategory } from '../../utils';
+// import { findItem, findItemsByCategory } from '../../utils';
 
 import Page from '../Page';
 import Card from '../Card';
@@ -14,9 +14,9 @@ import advices from '../../data/advices';
 import Button from '../Button';
 
 function ArticlePage() {
-  const { slug } = useParams();
+  // const { slug } = useParams();
 
-  const article = useSelector((state) => findItem(state.articles.data, slug));
+  // const article = useSelector((state) => findItem(state.articles.data, slug));
 
   // const advices = useSelector((state) =>
   //   findItemsByCategory(state.advices.data, name),
@@ -27,15 +27,25 @@ function ArticlePage() {
       <div className="article-page">
         <div className="article">
           <div className="article-elements">
-            <img src="image" alt="article-img" />
-            <div className="article-top">
-              <h2>Titre de l'article</h2>
+            <img
+              src="https://picsum.photos/id/183/300/450.jpg"
+              alt="titre"
+              className="article-elements-img"
+            />
+            <div className="article-elements-top">
+              <h2 className="article-title">Titre de l'article</h2>
               <span className="article-author">Jean Guy</span>
-              <time className="article-date" dateTime="2019-07-21">
-                21 juillet 2019
+              <time className="article-date" dateTime="2023-03-13">
+                13 mars 2023
               </time>
             </div>
-            <p className="article-text">lalalalaaaaaaaaaaaaaa</p>
+            <p className="article-elements-text">
+              Aliquid ut qui consequatur nobis perferendis. Vero eaque et
+              ducimus ut incidunt quo consequatur. Eos laboriosam laborum quo
+              aliquid qui non. Dignissimos atque quidem nemo occaecati dolorem
+              est dolores. Vitae et similique nulla voluptate itaque sit
+              excepturi.
+            </p>
           </div>
           <Button />
         </div>
