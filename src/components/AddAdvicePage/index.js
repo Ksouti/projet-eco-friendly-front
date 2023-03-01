c67c21320a6b17140db2031aeb3cf9eb9f9cd664
Alban Caré <alban.care@gmail.com>
2023-03-01 21:57:48 +0100

    REFACT: Fix & update style RWD

--- src/components/AddAdvicePage/index.js
+++ src/components/AddAdvicePage/index.js
@@ -7,12 +7,12 @@ import Button from '../Button';
 function AddAdvicePage() {
   return (
     <Page>
-      <div className="add-advice-page">
+      <div className="add-advice">
         <div className="title">Ajouter un conseil</div>
         <form>
-          <div className="category">
-            <select name="category" id="category">
-              <option value="" disabled selected hidden>
+          <div className="row">
+            <select name="category" id="category" className="advice-select">
+              <option value="" hidden>
                 Catégories
               </option>
               <option value="1">Mobilité</option>
@@ -20,12 +20,18 @@ function AddAdvicePage() {
               <option value="3">Santé</option>
               <option value="4">Énergie</option>
             </select>
+            <Input
+              type="text"
+              name="title"
+              color="primary"
+              placeholder="Titre"
+              value=""
+            />
           </div>
-          <Input type="text" name="title" color="primary" placeholder="Titre" />
-          <div className="richTextEditor">
-            <RichTextEditor />
-          </div>
+          <RichTextEditor />
           <div className="button-wrapper">
+            <Button color="primary">Publier</Button>
+            <Button>Sauvegarder</Button>
             <Button color="primary">Publier</Button>
             <Button>Sauvegarder</Button>
             <Button outline color="primary">
