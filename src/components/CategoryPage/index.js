7c7e409ea0ca9ff7bb4e45d0ad1287e063e10583
Alban Caré <alban.care@gmail.com>
2023-03-01 17:31:44 +0100

    FEAT: Dynamic tagline display based on the category
    - Get the list of categories in the state and get the current category to display the tagline
    - If categories don't exist in state, will use config.js file to update tagline

--- src/components/CategoryPage/index.js
+++ src/components/CategoryPage/index.js
@@ -2,7 +2,7 @@
 /* eslint-disable implicit-arrow-linebreak */
 import { useParams } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 
 import { findItemsByCategory } from '../../utils';
 import { loadingLastArticleData } from '../../actions/articles';
@@ -11,10 +11,15 @@ import Page from '../Page';
 import Card from '../Card';
 import Loader from '../Loader';
 
+import config from '../../config';
+
 import './styles.scss';
 
 function CategoryPage() {
   const { name } = useParams();
+  const [categories, setCategories] = useState(
+    useSelector((state) => state.common.categories),
+  );
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -33,13 +38,21 @@ function CategoryPage() {
     findItemsByCategory(state.articles.data, name),
   );
 
-  const article = useSelector((state) => state.articles.lastArticleData);
+  const lastArticle = useSelector((state) => state.articles.lastArticleData);
+
+  /* If there is no categories in the state, we set the default categories */
+  if (categories.length === 0) {
+    setCategories(config.defaultNavLinks);
+  }
+
+  /* Find the category in the categories array */
+  const category = categories.find((item) => item.slug === name);
 
   return (
     <Page>
       {articleIsLoaded ? (
         <div className="category-page">
-          <h1 className="category-sentence">La mobilité verte</h1>
+          <h1 className="category-sentence">{category.tagline}</h1>
           <div className="category-elements">
             <div className="advices">
               <h2 className="advices-sentence">Suivez vos conseils</h2>
@@ -58,10 +71,10 @@ function CategoryPage() {
             <div className="articles">
               <div className="articles-top">
                 <Card
-                  picture={article.picture}
-                  title={article.title}
-                  category={article.category}
-                  content={article.content}
+                  picture={lastArticle.picture}
+                  title={lastArticle.title}
+                  category={lastArticle.category}
+                  content={lastArticle.content}
                   format="horizontal"
                 />
               </div>
