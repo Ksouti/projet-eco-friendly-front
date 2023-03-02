7aed9b001b8c7f2156b02719dfd68c4b481c457a
AnoukFerrand <anoukfrd@gmail.com>
2023-03-02 18:11:27 +0100

    FEAT: update with Link AdvicesCardList composent

--- src/components/AdvicesCardsList/index.js
+++ src/components/AdvicesCardsList/index.js
@@ -3,7 +3,7 @@
 
 // !!! problems : les cards ne s'affichent pas
 
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 import { findItemsByCategory } from '../../utils';
@@ -24,13 +24,17 @@ function AdvicesCardsList() {
       <h2 className="advices-sentence">Suivez vos conseils</h2>
       <div className="advices-list">
         {advices.map((advice) => (
-          <div key={advice.id} className="advice-card">
+          <Link
+            to={`/conseils/${advice.slug}`}
+            key={advice.id}
+            className="advice-card"
+          >
             <Card
               title={advice.title}
               category={advice.category}
               content={advice.content}
             />
-          </div>
+          </Link>
         ))}
       </div>
     </div>
