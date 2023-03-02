6bc64686ff0bc9334d59529ad26cd005fda64ddf
Kitsu <97291210+Ksouti@users.noreply.github.com>
2023-03-02 15:44:01 +0100

    FIX: fixed height of the 404 and removal of the button

--- src/components/NotFoundPage/index.js
+++ src/components/NotFoundPage/index.js
@@ -1,15 +1,11 @@
 import './styles.scss';
 import Page from '../Page';
-import Button from '../Button';
 
 const NotFoundPage = () => (
   <Page>
     <div className="not-found-container">
       <h1>404</h1>
       <p>Page non trouvée</p>
-      <Button color="primary">
-        Retourner à l'accueil
-      </Button>
     </div>
   </Page>
 );
