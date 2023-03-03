15aab6cc23dab1257b0a38b7629bb504ff8f0fb9
Kitsu <97291210+Ksouti@users.noreply.github.com>
2023-03-03 10:54:00 +0100

    FEAT: add image to notFoundPage

--- src/components/NotFoundPage/index.js
+++ src/components/NotFoundPage/index.js
@@ -1,10 +1,11 @@
 import './styles.scss';
 import Page from '../Page';
+import logo from './notFound.png';
 
 const NotFoundPage = () => (
   <Page>
     <div className="not-found-container">
-      <h1>404</h1>
+      <img src={logo} alt="Logo" />;
       <p>Page non trouvée</p>
     </div>
   </Page>
