8c3abd3f04643ad37891411a64f94bb1cacf4c7f
Alban Caré <alban.care@gmail.com>
2023-03-03 17:13:46 +0100

    WIP: HomePage component update

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -1,38 +1,53 @@
-import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { loadingHomePageData } from '../../actions/common';
 
 import Page from '../Page';
 import Slider from '../Slider';
 import Card from '../Card';
+import Loader from '../Loader';
 
 import './styles.scss';
 
 function HomePage() {
-  const articles = useSelector((state) => state.articles.lastFourArticles);
-  const advices = useSelector((state) => state.advices.lastFourAdvices);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(loadingHomePageData());
+  }, []);
+
+  const homePageDataIsLoaded = useSelector(
+    (state) => state.common.homePageDataIsLoaded,
+  );
 
   return (
     <Page>
-      <div className="homepage">
-        <section className="advices">
-          <h2 className="advices-title">Suivez vos conseils</h2>
-          <div className="card-wrapper">
-            <div className="card-inner">
-              {advices.map((advice) => (
-                <Card
-                  key={advice.id}
-                  title={advice.title}
-                  content={advice.content}
-                  picture={advice.picture}
-                  category={advice.category}
-                />
-              ))}
+      {/* {homePageDataIsLoaded ? (
+        <div className="homepage">
+          <section className="advices">
+            <h2 className="advices-title">Suivez vos conseils</h2>
+            <div className="card-wrapper">
+              <div className="card-inner">
+                {advices.map((advice) => (
+                  <Card
+                    key={advice.id}
+                    title={advice.title}
+                    content={advice.content}
+                    picture={advice.picture}
+                    category={advice.category}
+                  />
+                ))}
+              </div>
             </div>
-          </div>
-        </section>
-        <section className="articles">
-          <Slider slides={articles} />
-        </section>
-      </div>
+          </section>
+          <section className="articles">
+            <Slider slides={articles} />
+          </section>
+        </div>
+      ) : (
+        <Loader />
+      )} */}
     </Page>
   );
 }
