9bde7780af3c62d0e3ccd8c08b9c1ca64fd020b2
Alban Caré <alban.care@gmail.com>
2023-03-03 14:59:40 +0100

    FEAT: Authorizations, controlled fields, publication of an advice

--- src/components/AddAdvicePage/index.js
+++ src/components/AddAdvicePage/index.js
@@ -1,7 +1,9 @@
 import { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { Navigate, useNavigate } from 'react-router-dom';
 
 import { OnInputChange } from '../../actions/common';
+import { userPublishNewAdvice } from '../../actions/user';
 
 import Page from '../Page';
 import Input from '../Field/Input';
@@ -15,46 +17,77 @@ import './styles.scss';
 
 function AddAdvicePage() {
   const dispatch = useDispatch();
-  const addTitle = useSelector((state) => state.advices.addTitle);
-  const addContent = useSelector((state) => state.advices.addContent);
+  const navigate = useNavigate();
 
+  /* check if user is logged */
+  const userIslogged = useSelector((state) => state.user.isLogged);
+  /* end check if user is logged */
+
+  /* if there is no advice, we redirect to the 404 page */
+  if (!userIslogged) {
+    return <Navigate to="/" replace />;
+  }
+
+  /* get state informations */
   const [categories, setCategories] = useState(
     useSelector((state) => state.common.categories),
   );
+  const userNickname = useSelector((state) => state.user.nickname);
+  /* end get state informations */
 
   /* If there is no categories in the state, we set the default categories */
   if (categories.length === 0) {
     setCategories(config.defaultNavLinks);
   }
 
-  const changeField = (value, identifier) => {
-    dispatch(OnInputChange(value, identifier));
-  };
+  // /* control input fields */
+  const title = useSelector((state) => state.advices.newAdviceTitle);
+  const category = useSelector((state) => state.advices.newAdviceCategory);
+  const content = useSelector((state) => state.advices.newAdviceContent);
+  // /* end control input fields */
 
+  /* change field value */
+  const changeField = (value) => {
+    dispatch(OnInputChange(value, 'newAdviceTitle'));
+  };
   const onSelectChange = (e) => {
-    dispatch(OnInputChange(e.target.value, 'addCategory'));
+    dispatch(OnInputChange(e.target.value, 'newAdviceCategory'));
   };
-
   const OnRichTextEditorChange = (e) => {
-    dispatch(OnInputChange(e, 'addContent'));
+    dispatch(OnInputChange(e, 'newAdviceContent'));
+  };
+  /* change field value */
+
+  /* Get the button name clicked */
+  const [buttonName, setButtonName] = useState(null);
+
+  /* submit form */
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (buttonName === 'publish') {
+      dispatch(userPublishNewAdvice());
+    }
+    if (buttonName === 'save') {
+      // dispatch(userSaveNewAdvice());
+    }
+    navigate(`/utilisateurs/${userNickname}`);
   };
 
   return (
     <Page>
       <div className="add-advice">
         <div className="title">Ajouter un conseil</div>
-        <form>
+        <form autoComplete="off" onSubmit={handleSubmit} method="POST">
           <div className="row">
             <select
-              name="addCategory"
-              id="category"
-              className="advice-select"
               onChange={onSelectChange}
+              value={category}
+              className="advice-select"
             >
               <option hidden>Catégories</option>
-              {categories.map((category) => (
-                <option key={category.id} value={category.id}>
-                  {category.name}
+              {categories.map((item) => (
+                <option key={item.id} value={item.id}>
+                  {item.name}
                 </option>
               ))}
             </select>
@@ -63,20 +96,28 @@ function AddAdvicePage() {
               name="addTitle"
               placeholder="Titre"
               onChange={changeField}
-              value={addTitle}
+              value={title}
               color="primary"
             />
           </div>
           <div className="text-editor">
             <RichTextEditor
               name="addContent"
-              value={addContent}
+              value={content}
               onChange={OnRichTextEditorChange}
             />
           </div>
           <div className="button-wrapper">
-            <Button color="primary">Publier</Button>
-            <Button>Sauvegarder</Button>
+            <Button
+              type="submit"
+              color="primary"
+              onclick={() => setButtonName('publish')}
+            >
+              Publier
+            </Button>
+            <Button type="submit" onclick={() => setButtonName('save')}>
+              Sauvegarder
+            </Button>
             <Button outline color="primary">
               Supprimer
             </Button>
