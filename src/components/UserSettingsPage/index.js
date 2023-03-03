99a4606532906423f5bc362c792bad64d3fa184f
Alban Caré <alban.care@gmail.com>
2023-03-03 10:33:02 +0100

    REFACT: Addition of the verification of the correspondence between the nickname passed in slug parameter and the user registered in state

--- src/components/UserSettingsPage/index.js
+++ src/components/UserSettingsPage/index.js
@@ -1,21 +1,33 @@
 import { useEffect } from 'react';
+import { Navigate, useParams } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 
 import Page from '../Page';
 import Advices from './Advices';
 import Account from './Account';
 
-import { loadingUserAdvices } from '../../actions/user';
+import { loadingUserAdvices, userLogout } from '../../actions/user';
 
 import './styles.scss';
 
 export default function UserSettingsPage() {
   const dispatch = useDispatch();
+  const { nickname } = useParams();
 
   /* get state informations */
   const user = useSelector((state) => state.user.data);
   const isLoadedAdvices = useSelector((state) => state.user.isLoadedAdvices);
   /* end get state informations */
+
+  /** check if the nickname is the same as the user's nickname
+   * if not :
+   *   remove all user's data from the state
+   *   redirect to the home page
+   */
+  if (nickname !== user.nickname) {
+    dispatch(userLogout());
+    return <Navigate to="/" replace />;
+  }
 
   /* Loading all user's advices */
   const advices = useSelector((state) => state.user.advices);
