a3774205b84f2dbe1b09f731d20c4e74a00563c0
Alban Caré <alban.care@gmail.com>
2023-03-06 10:04:10 +0100

    REFACT: Removing bindings to temporary data

--- src/middlewares/userMiddleware.js
+++ src/middlewares/userMiddleware.js
@@ -10,59 +10,38 @@ import {
 
 import config from '../config';
 
-import user from '../data/single-user'; // dev only
-
 const userMiddleware = (store) => (next) => (action) => {
   switch (action.type) {
     case USER_LOGIN:
-      if (config.env === 'dev') {
-        store.dispatch(userAuthenticationSuccess(user));
-      } else {
-        axios
-          .post(`${config.apiBaseUrl}/login_check`, {
-            username: store.getState().user.email,
-            password: store.getState().user.password,
-          })
-          .then((response) => {
-            store.dispatch(
-              userAuthenticationSuccess(
-                response.data.token,
-                response.data.user,
-              ),
-            );
-          })
-          .catch((error) => {
-            store.dispacth(userAuthenticationError(error));
-          });
-      }
+      axios
+        .post(`${config.apiBaseUrl}/login_check`, {
+          username: store.getState().user.email,
+          password: store.getState().user.password,
+        })
+        .then((response) => {
+          store.dispatch(
+            userAuthenticationSuccess(response.data.token, response.data.user),
+          );
+        })
+        .catch((error) => {
+          store.dispacth(userAuthenticationError(error));
+        });
       break;
     case USER_REGISTER:
-      if (config.env === 'dev') {
-        store.dispatch(
-          userRegisterSuccess({
-            email: store.getState().user.email,
-            password: store.getState().user.password,
-            nickname: store.getState().user.nickname,
-            firstname: store.getState().user.firstname,
-            lastname: store.getState().user.lastname,
-          }),
-        );
-      } else {
-        axios
-          .post(`${config.apiBaseUrl}/register`, {
-            email: store.getState().user.email,
-            password: store.getState().user.password,
-            nickname: store.getState().user.nickname,
-            firstname: store.getState().user.firstname,
-            lastname: store.getState().user.lastname,
-          })
-          .then((response) => {
-            store.dispatch(userRegisterSuccess(response.data));
-          })
-          .catch((error) => {
-            store.dispacth(userAuthenticationError(error));
-          });
-      }
+      axios
+        .post(`${config.apiBaseUrl}/register`, {
+          email: store.getState().user.email,
+          password: store.getState().user.password,
+          nickname: store.getState().user.nickname,
+          firstname: store.getState().user.firstname,
+          lastname: store.getState().user.lastname,
+        })
+        .then((response) => {
+          store.dispatch(userRegisterSuccess(response.data));
+        })
+        .catch((error) => {
+          store.dispacth(userAuthenticationError(error));
+        });
       break;
     default:
   }
