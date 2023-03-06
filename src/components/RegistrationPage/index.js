4fab9cf1245b174b1a5f99301b52b05b3f2b9dff
Alban Caré <alban.care@gmail.com>
2023-03-06 11:44:52 +0100

    FEAT: Realization of the registration page and the style

---
+++ src/components/RegistrationPage/index.js
@@ -0,0 +1,53 @@
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+
+import Page from '../Page';
+import Button from '../Button';
+
+import image from './image.png';
+
+import './styles.scss';
+
+function RegistrationPage() {
+  const navigate = useNavigate();
+  const isRegistered = useSelector((state) => state.user.isRegistered);
+  const nickname = useSelector((state) => state.user.nickname);
+  const email = useSelector((state) => state.user.email);
+
+  useEffect(() => {
+    if (!isRegistered) {
+      navigate('/');
+    }
+  }, [isRegistered, navigate]);
+
+  return (
+    <Page>
+      <div className="registration">
+        <h1 className="registration-title">
+          Merci <span>{nickname}</span> pour votre inscription !
+        </h1>
+        <p className="registration-text">
+          Nous sommes très heureux de vous compter parmi nous et vous souhaitons
+          la bienvenue !
+        </p>
+        <div className="registration-image">
+          <img src={image} alt="enregistrement" />
+        </div>
+        <p className="registration-text">
+          Un email vient de vous être envoyé sur l'adresse
+          <span> {email}</span>, vérifiez votre messagerie pour finaliser votre
+          enregistrement !
+        </p>
+        <p className="registration-text">
+          Vous n'avez pas reçu d'email ?
+          <Button link color="primary">
+            Renvoyer
+          </Button>
+        </p>
+      </div>
+    </Page>
+  );
+}
+
+export default RegistrationPage;
