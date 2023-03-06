56dd657482b11c52f9c418eb0cf413d16e7e4e88
AnoukFerrand <anoukfrd@gmail.com>
2023-03-06 16:08:10 +0100

    WIP: retrieving lost code, trying to make it work

--- src/components/ArticlePage/index.js
+++ src/components/ArticlePage/index.js
@@ -1,77 +1,60 @@
 /* eslint-disable function-paren-newline */
 /* eslint-disable implicit-arrow-linebreak */
-// import { useParams } from 'react-router-dom';
-// import { useSelector } from 'react-redux';
-
-// import { findItem, findItemsByCategory } from '../../utils';
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
 
 import Page from '../Page';
-import Card from '../Card';
 import AdvicesCardsList from '../AdvicesCardsList';
+import Loader from '../Loader';
+import { findItem } from '../../utils';
+
+// import Button from '../Button';
 
 import './styles.scss';
 
-import advices from '../../data/advices';
-import Button from '../Button';
-
 function ArticlePage() {
-  // const { slug } = useParams();
+  const { slug } = useParams();
 
-  // const article = useSelector((state) => findItem(state.articles.data, slug));
+  const article = useSelector((state) => findItem(state.articles.data, slug));
+  console.log(article);
 
-  // const advices = useSelector((state) =>
-  //   findItemsByCategory(state.advices.data, name),
-  // );
+  const advices = useSelector((state) => state.advices.data);
 
   return (
     <Page>
-      <div className="article-page">
-        <div className="article">
-          <div className="article-elements">
-            <img
-              src="https://picsum.photos/id/2/300/450.jpg"
-              alt="titre"
-              className="article-elements-img"
-            />
-            <div className="article-elements-top">
-              <h2 className="article-title">Titre de l'article</h2>
-              <span className="article-author">Jean Guy</span>
-              <time className="article-date" dateTime="2023-03-13">
-                13 mars 2023
-              </time>
+      {article ? (
+        <div className="article-page">
+          <div className="article">
+            <div className="article-elements">
+              <img
+                src={article.picture}
+                alt="titre"
+                className="article-elements-img"
+              />
+              <div className="article-elements-top">
+                <h2 className="article-title">{article.title}</h2>
+                <span className="article-author">
+                  {article.author.nickname}
+                </span>
+                <time className="article-date" dateTime="2023-03-13">
+                  {article.created_at}
+                </time>
+              </div>
+              <p className="article-elements-text">{article.content}</p>
             </div>
-            <p className="article-elements-text">
-              Aliquid ut qui consequatur nobis perferendis. Vero eaque et
-              ducimus ut incidunt quo consequatur. Eos laboriosam laborum quo
-              aliquid qui non. Dignissimos atque quidem nemo occaecati dolorem
-              est dolores. Vitae et similique nulla voluptate itaque sit
-              excepturi.
-            </p>
-          </div>
-          {/* <Button
+
+            {/* <Button
             type="button"
             className="article-button-return"
             // onClick="" aller à la page catégorie précédente
           /> */}
-        </div>
-
-        {/* <AdvicesCardsList /> */}
-        {/* without composant AdvicesCardsList :  */}
-        <div className="advices">
-          <h2 className="advices-sentence">Suivez vos conseils</h2>
-          <div className="advices-list">
-            {advices.map((advice) => (
-              <div key={advice.id} className="advice-card">
-                <Card
-                  title={advice.title}
-                  category={advice.category}
-                  content={advice.content}
-                />
-              </div>
-            ))}
           </div>
+
+          <AdvicesCardsList advices={advices} />
         </div>
-      </div>
+      ) : (
+        <Loader />
+      )}
     </Page>
   );
 }
