9d0e79ce28ce41d4e27c130d894640bbcde905f2
Amine Safouane <97291210+Ksouti@users.noreply.github.com>
2023-03-06 22:02:10 +0100

    FEAT: Improved AdvicePage component

--- src/components/AdvicePage/index.js
+++ src/components/AdvicePage/index.js
@@ -14,22 +14,24 @@ function AdvicePage() {
         <div className="advice">
           <div className="advice-elements">
             <div className="advice-elements-top">
-              <div className="advice-user">
-                <img src={navBarAvatar} alt="avatar" className="user-avatar" />
-                <div className="advice-author">Jean Guy,</div>
-                <time className="advice-date" dateTime="2023-03-13">
-                  13 mars 2023
-                </time>
+              <div className="advice-container">
+                <h2 className="advice-title">Titre du conseil</h2>
+                <div className="advice-user">
+                  <img src={navBarAvatar} alt="avatar" className="user-avatar" />
+                  <div className="advice-author">Jean Guy,</div>
+                  <time className="advice-date" dateTime="2023-03-13">
+                    13 mars 2023
+                  </time>
+                </div>
+                <p className="advice-elements-text">
+                  Aliquid ut qui consequatur nobis perferendis. Vero eaque et
+                  ducimus ut incidunt quo consequatur. Eos laboriosam laborum quo
+                  aliquid qui non. Dignissimos atque quidem nemo occaecati dolorem
+                  est dolores. Vitae et similique nulla voluptate itaque sit
+                  excepturi.
+                </p>
               </div>
-              <h2 className="advice-title">Titre du conseil</h2>
             </div>
-            <p className="advice-elements-text">
-              Aliquid ut qui consequatur nobis perferendis. Vero eaque et
-              ducimus ut incidunt quo consequatur. Eos laboriosam laborum quo
-              aliquid qui non. Dignissimos atque quidem nemo occaecati dolorem
-              est dolores. Vitae et similique nulla voluptate itaque sit
-              excepturi.
-            </p>
           </div>
         </div>
         <div className="advices">
