f423b87fc9d4d748823fa79d070b500b9f62dc58
Alban Caré <alban.care@gmail.com>
2023-03-08 21:38:27 +0100

    FEAT: Addiing control input fields

--- src/components/Form/UserSettingsForm/FormNickname.js
+++ src/components/Form/UserSettingsForm/FormNickname.js
@@ -1,19 +1,42 @@
+import { useDispatch, useSelector } from 'react-redux';
 import Input from '../../Field/Input';
 import Button from '../../Button';
 
+import {
+  userOnInputChange,
+  userSettingsUpdate,
+  userRemoveErrorMessages,
+} from '../../../actions/user';
+
 import './styles.scss';
 
-export default function FormNickname({ nickname }) {
-  let handleSubmit, handleChange, password;
+export default function FormNickname() {
+  const dispatch = useDispatch();
+
+  /* control input fields */
+  const nickname = useSelector((state) => state.user.nickname);
+  const password = useSelector((state) => state.user.password);
+
+  const changeField = (value, identifier) => {
+    console.log('changeField', value, identifier);
+    dispatch(userOnInputChange(value, identifier));
+  };
+
+  /* submit form */
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // dispatch(userRemoveErrorMessages());
+    // dispatch(userSettingsUpdate());
+  };
   return (
     <div className="modal-form nickename">
       <h5 className="title text-primary">Modifier mon pseudo</h5>
-      <form autoComplete="off" onSubmit="{handleSubmit}">
+      <form autoComplete="off" onSubmit={handleSubmit}>
         <Input
           type="text"
-          name={nickname}
+          name="nickname"
           placeholder="Pseudo"
-          onChange="{handleChange}"
+          onChange={changeField}
           value={nickname}
           color="primary"
         />
@@ -21,7 +44,7 @@ export default function FormNickname({ nickname }) {
           type="password"
           name="password"
           placeholder="Mot de passe"
-          onChange="{handleChange}"
+          onChange={changeField}
           value={password}
           color="primary"
         />
@@ -33,3 +56,10 @@ export default function FormNickname({ nickname }) {
     </div>
   );
 }
+
+// /* CLear identifier sessionStorage value */
+// if (sessionStorage.getItem('user')) {
+//   const user = JSON.parse(sessionStorage.getItem('user'));
+//   user[identifier] = '';
+//   sessionStorage.setItem('user', JSON.stringify(user));
+// }
