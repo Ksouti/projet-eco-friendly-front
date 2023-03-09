3591c5f816e4c79622318ec0cc9eedd83b30d68c
Alban Caré <alban.care@gmail.com>
2023-03-09 15:50:40 +0100

    REFACT: Improved if no tips were posted or the api doesn't return advice

--- src/components/UserSettingsPage/index.js
+++ src/components/UserSettingsPage/index.js
@@ -1,18 +1,14 @@
 import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
-
-import { getUserAdvices } from '../../actions/advices';
 
 import Page from '../Page';
 import UserAccount from './UserAccount/index';
 import UserAdvices from './UserAdvices/index';
-import Loader from '../Loader';
 
 import './styles.scss';
 
 export default function UserSettingsPage() {
-  const dispatch = useDispatch();
   const navigate = useNavigate();
 
   /* Check if user is logged */
@@ -23,24 +19,12 @@ export default function UserSettingsPage() {
     }
   }, [userIslogged]);
 
-  const isLoadedAdvices = useSelector((state) => state.advices.isLoadedAdvices);
-  /* Load user advices from API */
-  useEffect(() => {
-    if (!isLoadedAdvices) {
-      dispatch(getUserAdvices());
-    }
-  }, [isLoadedAdvices]);
-
   return (
     <Page>
-      {isLoadedAdvices ? (
-        <div className="settings">
-          <UserAccount />
-          <UserAdvices />
-        </div>
-      ) : (
-        <Loader />
-      )}
+      <div className="settings">
+        <UserAccount />
+        <UserAdvices />
+      </div>
     </Page>
   );
 }
