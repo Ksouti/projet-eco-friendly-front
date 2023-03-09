323ef5fad05c5023c5b4f0ed393efedd6f3abb62
Ksouti <ksuti@protonmail.com>
2023-03-09 18:12:08 +0100

    FIX: Fix AdvicePage

--- src/components/AdvicePage/index.js
+++ src/components/AdvicePage/index.js
@@ -1,10 +1,9 @@
 /* eslint-disable function-paren-newline */
 /* eslint-disable implicit-arrow-linebreak */
 
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
 import { useSelector } from 'react-redux';
 import { useParams } from 'react-router-dom';
+
 import { findItem } from '../../utils';
 
 import Page from '../Page';
@@ -14,29 +13,27 @@ import Loader from '../Loader';
 import './styles.scss';
 
 function AdvicePage() {
-  const dispatch = useDispatch();
   const { slug } = useParams();
-
-  /*
-  useEffect(() => {dispatch;
-  */
-  const advices = useSelector((state) => findItem(state.advices.data, slug));
-  console.log(advices);
+  const advice = useSelector((state) => findItem(state.advices.data, slug));
+  const advices = useSelector((state) => state.advices.data);
   return (
     <Page>
-      {advices ? (
+      {advices && advice ? (
         <div className="advice-page">
           <div className="advice">
             <div className="advice-elements">
               <div className="advice-elements-top">
                 <div className="advice-container">
-                  <h2 className="advice-title">{advices.title}</h2>
+                  <h2 className="advice-title">{advice.title}</h2>
                   <div className="advice-user">
-                    <img src={advices.contributor.avatar} alt="avatar" className="user-avatar" />
-                    <div className="advice-author">{advices.contributor.nickname},</div>
-                    <time className="advice-date" dateTime="2023-03-13">{advices.created_at}</time>
+                    <img src={advice.contributor.avatar} alt="avatar" className="user-avatar" />
+                    <div className="advice-author">{advice.contributor.nickname},</div>
+                    <time className="advice-date" dateTime="2023-03-13">{advice.created_at}</time>
                   </div>
-                  <p className="advice-elements-text">{advices.content}</p>
+                  <div
+                    dangerouslySetInnerHTML={{ __html: advice.content }}
+                    className="article-elements-text inner-html"
+                  />
                 </div>
               </div>
             </div>
