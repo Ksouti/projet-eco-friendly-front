414d858ca37c69e9da239884f56d7bea8d5064f8
Ksouti <ksuti@protonmail.com>
2023-03-10 11:43:45 +0100

    FIX: Updating AdvicePage

--- src/components/AdvicePage/index.js
+++ src/components/AdvicePage/index.js
@@ -24,16 +24,18 @@ function AdvicePage() {
             <div className="advice-elements">
               <div className="advice-elements-top">
                 <div className="advice-container">
-                  <h2 className="advice-title">{advice.title}</h2>
+                  <h1 className="advice-category">{advice.category.name}</h1>
                   <div className="advice-user">
                     <img src={advice.contributor.avatar} alt="avatar" className="user-avatar" />
                     <div className="advice-author">{advice.contributor.nickname},</div>
-                    <time className="advice-date" dateTime="2023-03-13">{advice.created_at}</time>
+                    <time className="advice-date">{advice.created_at}</time>
                   </div>
-                  <div
-                    dangerouslySetInnerHTML={{ __html: advice.content }}
-                    className="article-elements-text inner-html"
-                  />
+                  <div className="advice-title-container">
+                    <h2 className="advice-title">{advice.title}</h2>
+                    <div className="advice-content">
+                      <div dangerouslySetInnerHTML={{ __html: advice.content }} className="article-elements-text inner-html" />
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
