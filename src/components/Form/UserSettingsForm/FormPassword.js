5edc39f9d3afa08dce5b3328ff25fe20e445763e
Alban Caré <alban.care@gmail.com>
2023-03-10 13:33:24 +0100

    FEAT: Added User Password Change

--- src/components/Form/UserSettingsForm/FormPassword.js
+++ src/components/Form/UserSettingsForm/FormPassword.js
@@ -1,10 +1,14 @@
 /* eslint-disable brace-style */
+
 import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 import {
   userOnInputChange,
   userSendEmailVerification,
+  userToggleIsUpdated,
 } from '../../../actions/user';
+import { closeModal } from '../../../actions/common';
 
 import Input from '../../Field/Input';
 import Button from '../../Button';
@@ -13,6 +17,7 @@ import './styles.scss';
 
 export default function FormPassword() {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   /* control input fields */
   const confirmationEmail = useSelector(
@@ -23,22 +28,36 @@ export default function FormPassword() {
     dispatch(userOnInputChange(value, identifier));
   };
 
-  /* check if email is the same as the one is state */
-  /* if yes, send email with link to reset password */
-  /* if not, display error message */
+  /* Error messages */
+  const emailErrorMessages = useSelector(
+    (state) => state.user.emailErrorMessages,
+  );
+
+  /* submit form */
   const email = useSelector((state) => state.user.email);
 
   const handleSubmit = (e) => {
     e.preventDefault();
     if (confirmationEmail === email) {
       dispatch(userSendEmailVerification(confirmationEmail));
-    } else {
-      /* Afficher un message d'erreur les inforamtions saisies ne sont pas correct */
-      console.log('Email non envoyé');
     }
   };
 
-  /* **** */
+  /* clear confirmationEmail fileld */
+  const handleCancel = () => {
+    dispatch(userOnInputChange('', 'confirmationEmail'));
+    dispatch(closeModal());
+  };
+
+  /* Redirect user if is registring  */
+  const isRegitring = useSelector((state) => state.user.isRegitring);
+
+  if (isRegitring) {
+    dispatch(userToggleIsUpdated());
+    dispatch(closeModal());
+    navigate('/enregistrement', { replace: true });
+  }
+
   return (
     <div className="modal-form password">
       <h5 className="title text-primary">Modifier mon mot de passe</h5>
@@ -56,10 +75,21 @@ export default function FormPassword() {
           value={confirmationEmail}
           color="primary"
         />
+        {emailErrorMessages && emailErrorMessages.length > 0 && (
+          <div className="messages error-messages">
+            <ul>
+              {emailErrorMessages.map((item) => (
+                <li key={item}>{item}</li>
+              ))}
+            </ul>
+          </div>
+        )}
         <Button type="submit" color="primary">
           C'est parti !
         </Button>
-        <Button type="reset">Annuler</Button>
+        <Button type="reset" onclick={handleCancel}>
+          Annuler
+        </Button>
       </form>
     </div>
   );
